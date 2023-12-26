313d07641d5c2764ce32142ed473bad37727cd32
kashishdipro <kamanashishdatta@gmail.com>
2023-12-26 19:27:55 +0600

    fetching playlist from server

--- index.js
+++ index.js
@@ -11,29 +11,31 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kdtr5cm.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+const client = new MongoClient(uri, { 
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+
+async function run() {
+  try {
+    const playlistCollection = client.db('studyWithAnis').collection('playlist');
+    
+    // Client Get Playlist Api
+    app.get('/playlist', async(req, res) =>{
+        const query = {};
+        const cursor = playlistCollection.find(query);
+        const playlist = await cursor.toArray();
+        res.send(playlist);
+    })
+  } finally {
+  }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) =>{
     res.send('Study with Anis is running')
